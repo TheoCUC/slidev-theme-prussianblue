a76781e81352f1b69106e53c1bfa4eb83a6ba700
isaac <isaac@MacPolo.local>
2022-07-11 09:27:42 +0800

    Bug Fixed

--- components/contents.vue
+++ components/contents.vue
@@ -1,23 +1,24 @@
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script>
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  props: {
-    id: {
-      type: Number,
-      required: true,
-    }
-  },
-})
+	props: {
+		id: {
+			type: Number,
+			required: true,
+		},
+	},
+});
 </script>
 
 <template>
-  <div class="z-90 flex absolute bottom-0 w-full py-8px">
-    <span class="flex w-full">
-      <div v-for="(item, index) in $slidev.configs.contents" class="text-center flex-auto px-5">
-        <div v-if="id-1 == index" class="text-white">{{ item }}</div>
-        <div v-else class="text-blue-gray-500">{{ item }}</div>
-      </div>
-    </span>
-  </div>
+	<div class="z-90 flex absolute bottom-0 w-full py-8px">
+		<span class="flex w-full">
+			<div v-for="(item, index) in $slidev.configs.contents" class="text-center flex-auto px-5">
+				<div v-if="id - 1 == index" class="text-white">{{ item }}</div>
+				<div v-else class="text-blue-gray-500">{{ item }}</div>
+			</div>
+		</span>
+	</div>
 </template>
+
